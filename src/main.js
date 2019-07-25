2fb087db9858ccdccc4554a7a042db8cee46e8d0
Chalk <chalkpe@gmail.com>
2019-07-25 17:36:43 +0900

    add vuex

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import './firebase'
 
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
 import router from './router'
 import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   router,
   vuetify,
   render: h => h(App)
