d3420c7e8dfa77829acd2945775bc69f42e31fbe
Chalk <chalkpe@gmail.com>
2019-07-29 18:11:54 +0900

    add buttons

--- src/components/MamulList.vue
+++ src/components/MamulList.vue
@@ -2,15 +2,39 @@
   <v-container grid-list-lg>
     <v-layout wrap>
       <v-flex lg4 v-for="r in ranks" :key="r.value">
-        <v-card><v-list two-line subheader>
-          <v-subheader>{{ r.name }}</v-subheader>
-          <v-list-item v-for="m in getMamulByRank(r.value)" :key="m.name">
-            <v-list-item-content>
-              <v-list-item-title>{{ m.name }}</v-list-item-title>
-              <v-list-item-subtitle>{{ m.zone }}</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list></v-card>
+        <v-card>
+          <v-list two-line subheader>
+            <v-subheader>{{ r.name }}</v-subheader>
+            <v-list-item
+              link
+              v-for="m in getMamulByRank(r.value)"
+              :key="m.name"
+            >
+              <v-list-item-content>
+                <v-list-item-title>{{ m.name }}</v-list-item-title>
+                <v-list-item-subtitle>{{ m.zone }}</v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-action class="d-inline-block">
+                <v-tooltip top>
+                  <template v-slot:activator="{ on }">
+                    <v-btn icon small>
+                      <v-icon color="grey lighten-1" v-on="on">mdi-checkbox-marked-circle-outline</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>토벌 보고</span>
+                </v-tooltip>
+                <v-tooltip top>
+                  <template v-slot:activator="{ on }">
+                    <v-btn icon small>
+                      <v-icon color="grey lighten-1" v-on="on">mdi-help-circle-outline</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>실종 보고</span>
+                </v-tooltip>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
